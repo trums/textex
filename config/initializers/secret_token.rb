8fa5fb1e9245862477b6ffb6b3069d6b248fe567
trums <adrienne.polich@gmail.com>
2013-04-07 15:19:51 -0700

    fill in files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crucru::Application.config.secret_token = 'c8b5b8398a4df2102de5c9bec94de1f8b1e4899645241f88142aa09ae29ee5da02dea2d44c6f3de2a89e50fdcc2bb1e11afe705440d50dbf78a562e427a3a0a7'
