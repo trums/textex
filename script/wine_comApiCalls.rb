4a50b90ef34e7ea01577e6d256ed010aeb6e7151
trums <adrienne.polich@gmail.com>
2013-04-07 15:10:08 -0700

    initial commit

---
+++ script/wine_comApiCalls.rb
@@ -0,0 +1,11 @@
+# require 'open-uri'?? not sure if that gem is necessary
+
+''' This is code from codeacademy
+
+
+# Open http://placekitten.com/ for reading on line 4!
+kittens = open("http://placekitten.com")
+body = kittens.read[559, 441]
+
+# Add your puts statement below!
+puts body     ''''
